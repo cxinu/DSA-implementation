988410dc0b242dc4d664b3dfac94e762aca5e235
cxinu <cxinu3099@gmail.com>
2023-09-27 23:56:03 +0530

    Refactored queue lists

--- Stacks, Queues, and Lists/QueueLinkedLists.cpp
+++ Stacks, Queues, and Lists/QueueLinkedLists.cpp
@@ -15,56 +15,59 @@ class Queue {
   public:
     Queue() { front = rear = nullptr; }
 
-    void enqueue(int data) {
-        Node *newNode = new Node();
-        newNode->data = data;
-        newNode->next = nullptr;
+    void enqueue(int data);
+    int dequeue();
 
-        if (front == nullptr) {
-            front = rear = newNode;
-        } else {
-            rear->next = newNode;
-            rear = newNode;
-        }
-    }
-
-    bool isEmpty() { return front == nullptr; }
-
-    int dequeue() {
-        if (isEmpty()) {
-            cout << "Queue is empty." << endl;
-            return -1;
-        }
-
-        Node *temp = front;
-        int data = temp->data;
-
-        if (front == rear) {
-            front = rear = nullptr;
-        } else {
-            front = front->next;
-        }
-
-        delete temp;
-        return data;
-    }
-
-    void display() {
-        if (isEmpty()) {
-            cout << "Queue is empty." << endl;
-            return;
-        }
-
-        Node *temp = front;
-
-        while (temp != nullptr) {
-            cout << temp->data << " ";
-            temp = temp->next;
-        }
-
-        cout << endl;
-    }
+    void display();
 };
+
+void Queue::enqueue(int data) {
+    Node *newNode = new Node();
+    newNode->data = data;
+    newNode->next = nullptr;
+
+    if (front == nullptr) {
+        front = rear = newNode;
+    } else {
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+int Queue::dequeue() {
+    if (front == nullptr) {
+        cout << "Queue is empty." << endl;
+        return -1;
+    }
+
+    Node *temp = front;
+    int data = temp->data;
+
+    if (front == rear) {
+        front = rear = nullptr;
+    } else {
+        front = front->next;
+    }
+
+    delete temp;
+    return data;
+}
+
+void Queue::display() {
+    if (front == nullptr) {
+        cout << "Queue is empty." << endl;
+        return;
+    }
+
+    Node *temp = front;
+
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
 
 int main() {
     Queue queue;
