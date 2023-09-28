723315d2e2270506c674089dad8a3b1ba6b1a9da
cxinu <cxinu3099@gmail.com>
2023-09-28 16:29:18 +0530

    Refactor Doubly delete_key

--- Stacks, Queues, and Lists/DoublyLinkedLists.cpp
+++ Stacks, Queues, and Lists/DoublyLinkedLists.cpp
@@ -13,12 +13,22 @@ class DoublyLinkedList {
 
   public:
     DoublyLinkedList() { start = end = nullptr; }
+    ~DoublyLinkedList() {
+        while (start != nullptr) {
+            Node *temp = start;
+            start = start->next;
+            delete temp;
+        }
+        end = nullptr;
+    }
 
     void insert_begin(int value);
     void insert_end(int value);
     void insert_pos(int value, int position);
 
     void delete_key(int key);
+
+    void display();
 };
 
 void DoublyLinkedList::insert_begin(int value) {
@@ -53,7 +63,7 @@ void DoublyLinkedList::insert_end(int value) {
 
 void DoublyLinkedList::insert_pos(int value, int position) {
     if (position < 1) {
-        std::cout << "Invalid position\n";
+        cout << "Invalid position\n";
         return;
     }
 
@@ -69,7 +79,7 @@ void DoublyLinkedList::insert_pos(int value, int position) {
     }
 
     if (current == nullptr) {
-        std::cout << "Invalid position\n";
+        cout << "Invalid position\n";
         return;
     }
 
@@ -96,43 +106,66 @@ void DoublyLinkedList::delete_key(int key) {
     }
 
     if (current == nullptr) {
-        std::cout << "Key not found\n";
+        cout << "Key not found\n";
         return;
     }
 
-    if (current == start) {
+    if (start == end) {
+        start = end = NULL;
+    } else if (current == start) {
         start = current->next;
-
-        if (start != nullptr) {
-            start->prev = nullptr;
-        } else {
-            end = nullptr;
-        }
-
-        delete current;
-
-        return;
-    }
-
-    if (current == end) {
+        start->prev = NULL;
+    } else if (current == end) {
         end = current->prev;
-
-        if (end != nullptr) {
-            end->next = nullptr;
-        } else {
-            start = nullptr;
-        }
-
-        delete current;
-
-        return;
-    }
-
-    current->prev->next = current->next;
-
-    if (current->next != nullptr) {
+        end->next = NULL;
+    } else {
+        current->prev->next = current->next;
         current->next->prev = current->prev;
     }
 
     delete current;
+}
+
+void DoublyLinkedList::display() {
+    if (start == nullptr) {
+        cout << "List is empty" << endl;
+        return;
+    } else {
+        Node *temp = start;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+}
+
+int main() {
+    DoublyLinkedList list;
+
+    list.insert_end(5);
+    list.insert_end(15);
+    list.display();
+
+    list.insert_begin(0);
+    list.display();
+
+    list.insert_pos(9, 4);
+    list.display();
+
+    // delete key from middle
+    list.delete_key(15);
+    list.display();
+
+    // delete key from start
+    list.delete_key(0);
+    list.display();
+
+    // delete key from end
+    list.delete_key(9);
+    list.display();
+
+    // delete key from signle node
+    list.delete_key(5);
+    list.display();
 }
