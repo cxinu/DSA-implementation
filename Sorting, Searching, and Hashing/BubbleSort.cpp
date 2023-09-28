159172e2b01b5bb35aeb3f8613013db5178d15ad
cxinu <cxinu3099@gmail.com>
2023-09-28 22:36:34 +0530

    Add bubble sort

---
+++ Sorting, Searching, and Hashing/BubbleSort.cpp
@@ -0,0 +1,29 @@
+#include "Array.cpp"
+
+void Array::bubbleSort() {
+    for (int i = 0; i < size - 1; i++)
+        for (int j = 0; j < size - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+}
+
+int main() {
+    Array arr(7);
+    arr.insert(48);
+    arr.insert(28);
+    arr.insert(34);
+    arr.insert(89);
+    arr.insert(29);
+    arr.insert(93);
+    arr.insert(19);
+
+    arr.display();
+
+    arr.bubbleSort();
+    arr.display();
+
+    return 0;
+}
