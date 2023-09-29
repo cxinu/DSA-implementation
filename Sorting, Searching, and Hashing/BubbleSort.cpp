e2ea01ee9ab4815a73c42100ee3ab9ee20232848
cxinu <cxinu3099@gmail.com>
2023-09-29 07:20:16 +0530

    `{ }` LoL

--- Sorting, Searching, and Hashing/BubbleSort.cpp
+++ Sorting, Searching, and Hashing/BubbleSort.cpp
@@ -1,13 +1,15 @@
 #include "Array.cpp"
 
 void Array::bubbleSort() {
-    for (int i = 0; i < size - 1; i++)
-        for (int j = 0; j < size - i - 1; j++)
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
+        }
+    }
 }
 
 int main() {
