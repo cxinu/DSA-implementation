10c8584a410d0bd31d01c724b826e28550a57070
cxinu <cxinu3099@gmail.com>
2023-10-09 23:36:25 +0530

    Minor inconvenience

--- Sorting, Searching, and Hashing/SelectionSort.cpp
+++ Sorting, Searching, and Hashing/SelectionSort.cpp
@@ -2,12 +2,15 @@
 
 void Array::selectionSort() {
     for (int i = 0; i < size - 1; i++) {
+        int min = i;
         for (int j = i + 1; j < size; j++) {
-            if (arr[i] > arr[j]) {
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+            if (arr[min] > arr[j])
+                min = j;
+        }
+        if (min != i) {
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
     }
 }
